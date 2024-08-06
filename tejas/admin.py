a37efcc0bcc88838e359d04359f180deb6874012
saikumarbharat <33601067+saikumarbharat@users.noreply.github.com>
2024-08-06 19:41:52 +0530

    Update admin.py

--- tejas/admin.py
+++ tejas/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Post,Comment,ResearchPaper,Publisher,Author,Project
 
-
-
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status','created_on')
     list_filter = ("status",)
