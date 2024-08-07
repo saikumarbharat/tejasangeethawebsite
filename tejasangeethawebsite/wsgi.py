071184ad3b55340b302b59b4a853a810b0466a29
saikumarbharat <33601067+saikumarbharat@users.noreply.github.com>
2024-08-07 12:04:54 +0530

    Update wsgi.py

--- tejasangeethawebsite/wsgi.py
+++ tejasangeethawebsite/wsgi.py
@@ -12,7 +12,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'researchcenter.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tejasangeethawebsite.settings')
 os.environ["DJANGO_SETTINGS_MODULE"] = "tejasangeethawebsite.settings" 
 
 application = get_wsgi_application()
